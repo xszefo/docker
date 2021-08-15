45484941e6ad48d62b5ee062de923b9bd89e8653
xszefo <piotr.sumelka@gmail.com>
2021-08-15 12:17:38 +0200

    rabbitmq

---
+++ rabbitmq/rabbit_send_message.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import pika
+import random
+
+def send_message(message):
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbit'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='my-first-queue')
+
+    channel.basic_publish(exchange='', routing_key='my-first-queue', body=message)
+    print(f'Sent message: {message}')
+
+def main():
+    message = ''.join([str(random.randint(0,5)) for i in range(0,9)])
+    send_message(message)
+
+if __name__ == '__main__':
+    main()
